3b853e2a2c6779d5fa37494b8884014cb9e7e176
Ken Wilcox <wilcoxk@medamerica.com>
2015-04-27 12:33:13 -0600

    Add is working

---
+++ SimpleCalculator/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+  public interface ICalculator
+  {
+    String Calculate(String input);
+  }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Runtime.CompilerServices;
 
 namespace SimpleCalculator
 {
   class Program
   {
+    private CompositionContainer _container;
+
+    [Import(typeof(ICalculator))]
+    public ICalculator calculator;
+
+    private Program()
+    {
+      var catalog = new AggregateCatalog();
+      catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+
+      _container = new CompositionContainer(catalog);
+
+      try
+      {
+        this._container.ComposeParts(this);
+      }
+      catch (CompositionException compositionException)
+      {
+        Console.Error.WriteLine(compositionException.ToString());
+      }
+    }
+
     static void Main(string[] args)
     {
+      Program p = new Program();
+      String s;
+      Console.WriteLine("Enter Command: ");
+      while (true)
+      {
+        s = Console.ReadLine();
+        Console.WriteLine(p.calculator.Calculate(s));
+      }
     }
   }
 }
