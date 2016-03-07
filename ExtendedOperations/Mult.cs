dd91c2f6247679a911be3fda840fbacd387b2c93
Ken Wilcox <wilcoxk@medamerica.com>
2016-03-07 09:06:02 -0700

    Playing with MEF again

---
+++ ExtendedOperations/Mult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleCalculator;
+
+namespace ExtendedOperations
+{
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '*')]
+    class Mult: IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left * right;
+        }
+    }
+}
